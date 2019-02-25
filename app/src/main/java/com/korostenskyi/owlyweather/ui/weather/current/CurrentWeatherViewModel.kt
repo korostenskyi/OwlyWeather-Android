515780572a75852c60249498832a2f34a5901eec
Roman Korostenskyi <romankorostensky@gmail.com>
2019-02-25 09:12:45 +0200

    Implement the CurrentWeatherViewModels' logic

--- app/src/main/java/com/korostenskyi/owlyweather/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/korostenskyi/owlyweather/ui/weather/current/CurrentWeatherViewModel.kt
@@ -1,7 +1,26 @@
 package com.korostenskyi.owlyweather.ui.weather.current
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.korostenskyi.owlyweather.data.network.entity.OpenWeather.CurrentWeather
 import com.korostenskyi.owlyweather.data.repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherViewModel(private val repository: Repository): ViewModel() {
+
+    var currentWeather = MutableLiveData<CurrentWeather>()
+
+    init {
+        repository.currentWeather.observeForever { serverResponse ->
+            currentWeather.value = serverResponse
+        }
+    }
+
+    suspend fun fetchCurrentWeather(lat: Double, lon: Double) {
+        GlobalScope.launch(Dispatchers.IO) {
+            repository.fetchCurrentWeather(lat, lon)
+        }
+    }
 }
