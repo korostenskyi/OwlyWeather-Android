da63967f413423c5f23e0bc842b7090bf805d379
Roman Korostenskyi <romankorostensky@gmail.com>
2019-02-25 09:09:15 +0200

    Rework of DI. Move from Koin to Kodein

--- app/src/main/java/com/korostenskyi/owlyweather/OwlyWeatherApplication.kt
+++ app/src/main/java/com/korostenskyi/owlyweather/OwlyWeatherApplication.kt
@@ -1,13 +1,21 @@
 package com.korostenskyi.owlyweather
 
 import android.app.Application
+import com.korostenskyi.owlyweather.data.network.OpenWeatherDataSourceImpl
+import com.korostenskyi.owlyweather.data.network.api.OpenWeatherApiService
+import com.korostenskyi.owlyweather.data.repository.RepositoryImpl
 import com.korostenskyi.owlyweather.di.appModule
-import org.koin.android.ext.android.startKoin
+import com.korostenskyi.owlyweather.ui.weather.current.CurrentWeatherViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.androidModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
 
-class OwlyWeatherApplication: Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        startKoin(this, listOf(appModule))
+class OwlyWeatherApplication: Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        import(appModule)
     }
 }
--- app/src/main/java/com/korostenskyi/owlyweather/di/AppModule.kt
+++ app/src/main/java/com/korostenskyi/owlyweather/di/AppModule.kt
@@ -1,16 +1,26 @@
 package com.korostenskyi.owlyweather.di
 
-import com.korostenskyi.owlyweather.data.repository.Repository
+import com.korostenskyi.owlyweather.data.network.OpenWeatherDataSourceImpl
+import com.korostenskyi.owlyweather.data.network.api.OpenWeatherApiService
 import com.korostenskyi.owlyweather.data.repository.RepositoryImpl
-import com.korostenskyi.owlyweather.ui.weather.current.CurrentWeatherViewModel
-import org.koin.androidx.viewmodel.ext.koin.viewModel
-import org.koin.dsl.module.module
+import com.korostenskyi.owlyweather.ui.weather.current.CurrentWeatherViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
 
-val appModule = module {
+val appModule = Kodein.Module("app") {
 
-    // CurrentWeather repository
-    single<Repository> { RepositoryImpl() }
+    // OpenWeatherApiService
+    bind() from singleton { OpenWeatherApiService() }
 
-    // CurrentWeather ViewModel
-    viewModel {CurrentWeatherViewModel(get())}
+    // OpenWeatherDataSourceImpl
+    bind() from singleton { OpenWeatherDataSourceImpl(instance()) }
+
+    // OpenWeatherRepositoryImpl
+    bind() from singleton { RepositoryImpl(instance()) }
+
+    // CurrentWeatherViewModelFactory
+    bind() from provider { CurrentWeatherViewModelFactory(instance()) }
 }
