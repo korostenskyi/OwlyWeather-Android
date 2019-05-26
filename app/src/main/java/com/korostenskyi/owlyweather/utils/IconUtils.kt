b77835fcb792f5205c1b0e70b4ad9cd0f3a48dd5
Roman Korostenskyi <romankorostensky@gmail.com>
2019-05-26 21:33:00 +0300

    Refactor IconUtil

--- app/src/main/java/com/korostenskyi/owlyweather/utils/IconUtils.kt
+++ app/src/main/java/com/korostenskyi/owlyweather/utils/IconUtils.kt
@@ -9,22 +9,11 @@ object IconUtils {
             "01d" -> R.drawable.ic_01d
             "01n" -> R.drawable.ic_01n
             "02d" -> R.drawable.ic_02d
-            "02n" -> R.drawable.ic_03d
-            "03d" -> R.drawable.ic_03d
-            "03n" -> R.drawable.ic_03d
-            "04d" -> R.drawable.ic_03d
-            "04n" -> R.drawable.ic_03d
-            "09d" -> R.drawable.ic_09d
-            "09n" -> R.drawable.ic_09d
-            "10d" -> R.drawable.ic_09d
-            "10n" -> R.drawable.ic_09d
-            "11d" -> R.drawable.ic_11d
-            "11n" -> R.drawable.ic_11d
-            "13d" -> R.drawable.ic_13d
-            "13n" -> R.drawable.ic_13d
-            "50d" -> R.drawable.ic_50d
-            "50n" -> R.drawable.ic_50d
-            else -> R.drawable.ic_01d
+            "02n", "03d", "03n", "04d", "04n" -> R.drawable.ic_03d
+            "09d", "09n", "10d", "10n" -> R.drawable.ic_09d
+            "11d", "11n" -> R.drawable.ic_11d
+            "13d", "13n" -> R.drawable.ic_13d
+            else -> R.drawable.ic_50d
         }
     }
 }
